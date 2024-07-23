1f643be7e31b74f02e2485f1eefe1d0ac310f59a
Todos Por Um <anaemarcos1337@gmail.com>
2024-07-23 00:55:32 -0300

    Create Atividade.java

---
+++ Atividade.java
@@ -0,0 +1,13 @@
+ import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.security.Key;
+import javax.crypto.SecretKey;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextText;
+    private Button buttonEncrypt
